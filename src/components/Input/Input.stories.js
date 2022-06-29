193f45fb9d486b7545502cac1a24fd3d37cdfd8e
dkmqflx <dkmqflx@gmail.com>
2022-06-29 23:49:02 +0900

    4.1 Story Heirarchy

---
+++ src/components/Input/Input.stories.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Input from './Input';
+
+export default {
+  title: 'Form/Input', // heirarych를 설정할 수 있다.
+  component: Input,
+};
+
+export const Small = () => <Input size="small" placeholder="Small size" />;
+export const Medium = () => <Input size="medium" placeholder="Medium size" />;
+export const Large = () => <Input size="large" placeholder="Large size" />;
